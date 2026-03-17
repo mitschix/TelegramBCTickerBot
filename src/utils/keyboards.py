6d72d79f6ad9961087ac9a6534ca864227d077aa
mitschix <23629789+mitschix@users.noreply.github.com>
2026-03-17 23:36:46 +0100

    refactor: move keyboards to subdir

--- src/utils/keyboards.py
+++ src/utils/keyboards.py
@@ -1,27 +1,29 @@
-from btc_query import CURR_LIST
 from telegram import InlineKeyboardButton
 
-# setting up the inline keyboards
+from utils.btc_query import CURR_LIST
+
 keyboard_curr_set = [
-    [InlineKeyboardButton(curr, callback_data="chdef_{}".format(curr))]
-    for curr in CURR_LIST
+    [InlineKeyboardButton(curr, callback_data=f"chdef_{curr}")] for curr in CURR_LIST
 ]
-keyboard_curr_set += ([InlineKeyboardButton("Go Back", callback_data="main_btc")],)
+keyboard_curr_set.append([InlineKeyboardButton("Go Back", callback_data="main_btc")])
+
 keyboard_curr_get = [
-    [InlineKeyboardButton(curr, callback_data="getbtc_{}".format(curr))]
-    for curr in CURR_LIST
+    [InlineKeyboardButton(curr, callback_data=f"getbtc_{curr}")] for curr in CURR_LIST
 ]
-keyboard_curr_get += ([InlineKeyboardButton("Go Back", callback_data="main_btc")],)
+keyboard_curr_get.append([InlineKeyboardButton("Go Back", callback_data="main_btc")])
+
 keyboard_main = [
     [InlineKeyboardButton("Ticker", callback_data="main_ticker")],
     [InlineKeyboardButton("Bitcoin", callback_data="main_btc")],
     [InlineKeyboardButton("User", callback_data="main_user")],
 ]
+
 keyboard_btc = [
     [InlineKeyboardButton("Get Bitcoin", callback_data="btc_getbtc")],
     [InlineKeyboardButton("Get Bitcoin Default", callback_data="btc_getbtcdef")],
     [InlineKeyboardButton("Go Back", callback_data="main_main")],
 ]
+
 keyboard_ticker = [
     [InlineKeyboardButton("Show borders", callback_data="ticker_showborder")],
     [
@@ -32,10 +34,12 @@ keyboard_ticker = [
     [InlineKeyboardButton("Set borders", callback_data="ticker_setborder")],
     [InlineKeyboardButton("Go Back", callback_data="main_main")],
 ]
+
 keyboard_no_user = [
     [InlineKeyboardButton("Bitcoin", callback_data="main_btc")],
     [InlineKeyboardButton("Go Main Menu", callback_data="main_main")],
 ]
+
 keyboard_user = [
     [InlineKeyboardButton("Change Default Currency", callback_data="user_chdef")],
     [InlineKeyboardButton("Set Bitcoin amount", callback_data="user_setcurrent")],
@@ -53,11 +57,3 @@ KEYBOARDS = {
     "no_user": keyboard_no_user,
     "user": keyboard_user,
 }
-
-
-def main() -> None:
-    print("helper script to allfbot.py")
-
-
-if __name__ == "__main__":
-    main()
